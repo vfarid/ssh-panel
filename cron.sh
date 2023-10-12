8ef6ab2bbfed9179069e1aba357b5b018ad39f89
Vahid Farid <farid@aradsolution.com>
2023-10-12 17:12:16 +0330

    Update cron.sh

--- cron.sh
+++ cron.sh
@@ -4,4 +4,4 @@ TIMEOUT=600
 TIMESTAMP=`date +%Y-%m-%d-%H-%M`
 OUTPUT=/var/log/ssh-panel/$TIMESTAMP.log
 
-sh -ic "{ /usr/sbin/nethogs -t | grep 'sshd:' &> $OUTPUT; kill 0; } | { sleep $TIMEOUT; kill 0; }" 3>&1 2>/dev/null
+sh -ic "{ stdbuf -oL nethogs -t | grep 'sshd:' &> $OUTPUT; kill 0; } | { sleep $TIMEOUT; kill 0; }" 3>&1 2>/dev/null
